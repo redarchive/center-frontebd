d2425b3c354d19104b88bb9eac572ab347c4b476
gimmuil <k74800601@gmail.com>
2022-12-15 19:53:23 +0900

    몰루

--- src/components/main/item/item-textbox/index.tsx
+++ src/components/main/item/item-textbox/index.tsx
@@ -8,13 +8,13 @@ const Textbox = ({ data }: any): JSX.Element => {
   return (
     <div className={style.item__textbox}>
       <div className={style.title}>설명</div>
-      <div className={style.text__content}>
-        <MarkdownView markdown={xss(data.description)}/>
-      </div>
       <div className={style.tags}>
         {data.tags.map((v: any, i: number) => (
           <span key={i}>#{v.label}</span>
         ))}
+      </div>
+      <div className={style.text__content}>
+        <MarkdownView markdown={xss(data.description)}/>
       </div>
       <div className={style.day}>
         <div>업로드 날짜</div>
